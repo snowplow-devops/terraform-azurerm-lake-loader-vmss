dfccacb87be8950fe94e1d90b7570fa39a32c7e3
jbeemster <joshua.a.beemster@gmail.com>
2023-06-30 11:20:11 +1000

    Add lake loader deployment on VMSS

---
+++ main.tf
@@ -0,0 +1,243 @@
+locals {
+  module_name    = "lake-loader-vmss"
+  module_version = "0.1.0"
+
+  app_name = "lake-loader-azure"
+  # TODO: Change once 0.1.0 is published
+  app_version = "0.1.0-rc2"
+
+  local_tags = {
+    Name           = var.name
+    app_name       = local.app_name
+    app_version    = local.app_version
+    module_name    = local.module_name
+    module_version = local.module_version
+  }
+
+  tags = merge(
+    var.tags,
+    local.local_tags
+  )
+}
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+module "telemetry" {
+  source  = "snowplow-devops/telemetry/snowplow"
+  version = "0.5.0"
+
+  count = var.telemetry_enabled ? 1 : 0
+
+  user_provided_id = var.user_provided_id
+  cloud            = "AZURE"
+  region           = data.azurerm_resource_group.rg.location
+  app_name         = local.app_name
+  app_version      = local.app_version
+  module_name      = local.module_name
+  module_version   = local.module_version
+}
+
+# --- Network: Security Group Rules
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = var.resource_group_name
+
+  tags = local.tags
+}
+
+resource "azurerm_network_security_rule" "ingress_tcp_22" {
+  name                        = "${var.name}_ingress_tcp_22"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefixes     = var.ssh_ip_allowlist
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "egress_tcp_80" {
+  name                        = "${var.name}_egress_tcp_80"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "egress_tcp_443" {
+  name                        = "${var.name}_egress_tcp_443"
+  priority                    = 101
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+# Needed for clock synchronization
+resource "azurerm_network_security_rule" "egress_udp_123" {
+  name                        = "${var.name}_egress_udp_123"
+  priority                    = 102
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Udp"
+  source_port_range           = "*"
+  destination_port_range      = "123"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+# --- IAM: Authentication & Credentials
+
+# Lookup current user
+data "azuread_client_config" "current" {}
+
+resource "azuread_application" "app_registration" {
+  display_name = "${var.name}-app"
+  # Assign current user as owner, otherwise we won't be able to modify or delete after creation (Active directory admins can also modify)
+  owners = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_application_password" "app_pasword" {
+  application_object_id = azuread_application.app_registration.object_id
+}
+
+resource "azuread_service_principal" "sp" {
+  application_id = azuread_application.app_registration.application_id
+  use_existing   = true
+}
+
+# Look up our container's resource ID
+data "azurerm_storage_container" "sc" {
+  name                 = var.storage_container_name
+  storage_account_name = var.storage_account_name
+}
+
+resource "azurerm_role_assignment" "sc_app_ra" {
+  scope                = data.azurerm_storage_container.sc.resource_manager_id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azuread_service_principal.sp.object_id
+}
+
+# --- EventHubs: Consumer Groups
+
+resource "azurerm_eventhub_consumer_group" "enriched_topic" {
+  name = var.name
+
+  namespace_name      = var.eh_namespace_name
+  eventhub_name       = var.enriched_topic_name
+  resource_group_name = var.resource_group_name
+}
+
+# --- Compute: VM scale-set deployment
+
+locals {
+  resolvers_raw = concat(var.default_iglu_resolvers, var.custom_iglu_resolvers)
+
+  resolvers_public = [
+    for resolver in local.resolvers_raw : merge(
+      {
+        name           = resolver["name"],
+        priority       = resolver["priority"],
+        vendorPrefixes = resolver["vendor_prefixes"],
+        connection = {
+          http = {
+            uri = resolver["uri"]
+          }
+        }
+      }
+    ) if resolver["api_key"] == ""
+  ]
+
+  resolvers_private = [
+    for resolver in local.resolvers_raw : merge(
+      {
+        name           = resolver["name"],
+        priority       = resolver["priority"],
+        vendorPrefixes = resolver["vendor_prefixes"],
+        connection = {
+          http = {
+            uri    = resolver["uri"]
+            apikey = resolver["api_key"]
+          }
+        }
+      }
+    ) if resolver["api_key"] != ""
+  ]
+
+  resolvers = flatten([
+    local.resolvers_public,
+    local.resolvers_private
+  ])
+
+  iglu_config = templatefile("${path.module}/templates/iglu_config.json.tmpl", { resolvers = jsonencode(local.resolvers) })
+
+  config = templatefile("${path.module}/templates/config.hocon.tmpl", {
+    enriched_topic_name              = var.enriched_topic_name
+    enriched_topic_connection_string = var.enriched_topic_connection_string
+    enriched_group_id                = azurerm_eventhub_consumer_group.enriched_topic.name
+    bad_topic_name                   = var.bad_topic_name
+    bad_topic_connection_string      = var.bad_topic_connection_string
+    eh_namespace_broker              = var.eh_namespace_broker
+
+    storage_account_name   = var.storage_account_name
+    storage_container_name = var.storage_container_name
+    storage_container_path = var.storage_container_path
+  })
+
+  user_data = templatefile("${path.module}/templates/user-data.sh.tmpl", {
+    tenant_id     = azuread_service_principal.sp.application_tenant_id
+    client_id     = azuread_application.app_registration.application_id
+    client_secret = azuread_application_password.app_pasword.value
+
+    config      = local.config
+    iglu_config = local.iglu_config
+    version     = local.app_version
+
+    telemetry_script = join("", module.telemetry.*.azurerm_ubuntu_22_04_user_data)
+
+    java_opts = var.java_opts
+
+    # TODO: Remove me when image goes public!
+    dockerhub_username     = var.dockerhub_username
+    dockerhub_access_token = var.dockerhub_access_token
+  })
+}
+
+module "service" {
+  source  = "snowplow-devops/service-vmss/azurerm"
+  version = "0.1.0"
+
+  user_supplied_script = local.user_data
+  name                 = var.name
+  resource_group_name  = var.resource_group_name
+
+  subnet_id                   = var.subnet_id
+  network_security_group_id   = azurerm_network_security_group.nsg.id
+  associate_public_ip_address = var.associate_public_ip_address
+  admin_ssh_public_key        = var.ssh_public_key
+
+  sku            = var.vm_sku
+  instance_count = var.vm_instance_count
+
+  tags = local.tags
+}
---
+++ variables.tf
@@ -0,0 +1,172 @@
+variable "name" {
+  description = "A name which will be pre-pended to the resources created"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group to deploy the service into"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The subnet id to deploy the service into"
+  type        = string
+}
+
+variable "vm_sku" {
+  description = "The instance type to use"
+  type        = string
+  default     = "Standard_B2s"
+}
+
+variable "vm_instance_count" {
+  description = "The instance count to use"
+  type        = number
+  default     = 1
+}
+
+variable "associate_public_ip_address" {
+  description = "Whether to assign a public ip address to this instance"
+  type        = bool
+  default     = true
+}
+
+variable "ssh_public_key" {
+  description = "The SSH public key attached for access to the servers"
+  type        = string
+}
+
+variable "ssh_ip_allowlist" {
+  description = "The comma-seperated list of CIDR ranges to allow SSH traffic from"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "tags" {
+  description = "The tags to append to this resource"
+  default     = {}
+  type        = map(string)
+}
+
+variable "java_opts" {
+  description = "Custom JAVA Options"
+  default     = "-Dorg.slf4j.simpleLogger.defaultLogLevel=info -XX:MinRAMPercentage=50 -XX:MaxRAMPercentage=75"
+  type        = string
+}
+
+# --- Configuration options
+
+variable "enriched_topic_name" {
+  description = "The name of the enriched Event Hubs topic that the loader will pull data from"
+  type        = string
+}
+
+variable "enriched_topic_connection_string" {
+  description = "The connection string to use for reading from the enriched topic"
+  type        = string
+}
+
+variable "bad_topic_name" {
+  description = "The name of the bad Event Hubs topic that the loader will push failures into"
+  type        = string
+}
+
+variable "bad_topic_connection_string" {
+  description = "The connection string to use for writing to the bad topic"
+  type        = string
+}
+
+variable "eh_namespace_name" {
+  description = "The name of the Event Hubs namespace"
+  type        = string
+}
+
+variable "eh_namespace_broker" {
+  description = "The broker to configure for access to the Event Hubs namespace"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Name of the output storage account"
+  type        = string
+}
+
+variable "storage_container_name" {
+  description = "Name of the output storage container"
+  type        = string
+}
+
+variable "storage_container_path" {
+  description = "Path within the storage container to write output into"
+  type        = string
+  default     = "events/"
+}
+
+# TODO: Remove me when image goes public!
+variable "dockerhub_username" {
+  description = "Username for logging into DockerHub to download the loader image"
+  type        = string
+  sensitive   = true
+}
+
+# TODO: Remove me when image goes public!
+variable "dockerhub_access_token" {
+  description = "Access token for logging into DockerHub to download the loader image"
+  type        = string
+  sensitive   = true
+}
+
+# --- Iglu Resolver
+
+variable "default_iglu_resolvers" {
+  description = "The default Iglu Resolvers that will be used by Enrichment to resolve and validate events"
+  default = [
+    {
+      name            = "Iglu Central"
+      priority        = 10
+      uri             = "http://iglucentral.com"
+      api_key         = ""
+      vendor_prefixes = []
+    },
+    {
+      name            = "Iglu Central - Mirror 01"
+      priority        = 20
+      uri             = "http://mirror01.iglucentral.com"
+      api_key         = ""
+      vendor_prefixes = []
+    }
+  ]
+  type = list(object({
+    name            = string
+    priority        = number
+    uri             = string
+    api_key         = string
+    vendor_prefixes = list(string)
+  }))
+}
+
+variable "custom_iglu_resolvers" {
+  description = "The custom Iglu Resolvers that will be used by Enrichment to resolve and validate events"
+  default     = []
+  type = list(object({
+    name            = string
+    priority        = number
+    uri             = string
+    api_key         = string
+    vendor_prefixes = list(string)
+  }))
+}
+
+# --- Telemetry
+
+variable "telemetry_enabled" {
+  description = "Whether or not to send telemetry information back to Snowplow Analytics Ltd"
+  type        = bool
+  default     = true
+}
+
+variable "user_provided_id" {
+  description = "An optional unique identifier to identify the telemetry events emitted by this stack"
+  type        = string
+  default     = ""
+}
